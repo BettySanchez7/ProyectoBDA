d3b0476e08cd068079057b3b8c57c0496215db4a
Carlos Morales <carlosgl.moralestz@gmail.com>
2020-06-11 14:23:44 -0500

    Creacion de base de datos

---
+++ s-01-creacion-bd.sql
@@ -0,0 +1,51 @@
+create database cgmtbd2
+user sys identified by system2
+user system identified by system2
+logfile group 1 (
+    '/u01/app/oracle/oradata/MOSAproy/disk_1/redo01a.log',
+    '/u01/app/oracle/oradata/MOSAproy/disk_2/redo01b.log',
+    '/u01/app/oracle/oradata/MOSAproy/disk_3/redo01c.log') size 50m blocksize 512,
+group 2 (
+    '/u01/app/oracle/oradata/MOSAproy/disk_1/redo02a.log',
+    '/u01/app/oracle/oradata/MOSAproy/disk_2/redo02b.log',
+    '/u01/app/oracle/oradata/MOSAproy/disk_3/redo02c.log') size 50m blocksize 512,
+group 3 (
+    '/u01/app/oracle/oradata/MOSAproy/disk_1/redo03a.log',
+    '/u01/app/oracle/oradata/MOSAproy/disk_2/redo03b.log',
+    '/u01/app/oracle/oradata/MOSAproy/disk_3/redo03c.log') size 50m blocksize 512
+maxloghistory 1
+maxlogfiles 16
+maxlogmembers 3
+maxdatafiles 1024
+character set AL32UTF8
+national character set AL16UTF16
+extent management local
+datafile '/u01/app/oracle/oradata/MOSAproy/disk_1/system01.dbf'
+size 700m reuse autoextend on next 10240k maxsize unlimited
+sysaux datafile '/u01/app/oracle/oradata/MOSAproy/disk_1/sysaux01.dbf'
+size 550m reuse autoextend on next 10240k maxsize unlimited
+default tablespace users
+datafile '/u01/app/oracle/oradata/MOSAproy/disk_1/users01.dbf'
+size 500m reuse autoextend on maxsize unlimited
+default temporary tablespace tempts1
+tempfile '/u01/app/oracle/oradata/MOSAproy/disk_1/temp01.dbf'
+size 20m reuse autoextend on next 640k maxsize unlimited
+undo tablespace undotbs1
+datafile '/u01/app/oracle/oradata/MOSAproy/disk_1/undotbs01.dbf'
+size 200m reuse autoextend on next 5120k maxsize unlimited
+user_data tablespace usertbs
+datafile '/u01/app/oracle/oradata/MOSAproy/disk_1/usertbs01.dbf'
+size 200m reuse autoextend on maxsize unlimited;
+alter user sys identified by system2;
+alter user system identified by system2;
+
+create tablespace apps_tbs logging
+datafile '/u01/app/oracle/oradata/MOSAproy/disk_1//apps01.dbf'
+size 500m reuse autoextend on next 1280k maxsize unlimited
+extent management local;
+-- create a tablespace for indexes, separate from user tablespace
+--(optional)
+create tablespace indx_tbs logging
+datafile '/u01/app/oracle/oradata/MOSAproy/disk_1/indx01.dbf'
+size 100m reuse autoextend on next 1280k maxsize unlimited
+extent management local;
