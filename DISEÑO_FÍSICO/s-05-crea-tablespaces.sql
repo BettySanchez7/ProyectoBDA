92ee8c8aa1e444be34f12ebc650e11f33ea58c02
Carlos Morales <carlosgl.moralestz@gmail.com>
2020-06-18 13:13:51 -0500

    Tablespace para los índices

--- DISEÑO_FÍSICO/s-05-crea-tablespaces.sql
+++ DISEÑO_FÍSICO/s-05-crea-tablespaces.sql
@@ -24,10 +24,19 @@ create tablespace bibliotecas_tbs
     segment space management auto
 ;
 
+
+create tablespace indx_tbs logging
+    datafile '/u01/app/oracle/oradata/MOSAproy/disk_1/indx01.dbf' size 100m,
+             '/u01/app/oracle/oradata/MOSAproy/disk_2/indx02.dbf' size 100m,
+             '/u01/app/oracle/oradata/MOSAproy/disk_3/indx03.dbf' size 100m
+    autoextend on next 1280k maxsize unlimited
+    extent management local
+;
+
 create tablespace lobs_tbs
-    datafile '/u01/app/oracle/oradata/MOSAproy/disk_1/lobs_tbs.dbf' size 1000m,  --AJUSTAR TAMAÑO
-             '/u02/app/oracle/oradata/MOSAproy/disk_2/lobs_tbs.dbf' size 1000m,
-             '/u03/app/oracle/oradata/MOSAproy/disk_3/lobs_tbs.dbf' size 1000m
+    datafile '/u01/app/oracle/oradata/MOSAproy/disk_1/lobs_tbs01.dbf' size 1000m,  --AJUSTAR TAMAÑO
+             '/u02/app/oracle/oradata/MOSAproy/disk_2/lobs_tbs02.dbf' size 1000m,
+             '/u03/app/oracle/oradata/MOSAproy/disk_3/lobs_tbs03.dbf' size 1000m
     autoextent on next 100m maxsize unlimited
     extent management local autoallocate
     segment space management auto
