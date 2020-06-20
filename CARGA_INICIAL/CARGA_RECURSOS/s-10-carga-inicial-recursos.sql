9826c871116cef87639daec43aa67e5dfea8e7a2
BettySanchez7 <31338064+BettySanchez7@users.noreply.github.com>
2020-06-20 14:07:46 -0500

    Update s-10-carga-inicial-recursos.sql

--- CARGA_INICIAL/CARGA_RECURSOS/s-10-carga-inicial-recursos.sql
+++ CARGA_INICIAL/CARGA_RECURSOS/s-10-carga-inicial-recursos.sql
@@ -11,16 +11,29 @@ Prompt realizando la carga de datos
 @@s-10-autor.sql
 @@s-10-editorial.sql
 @@s-10-recurso-status.sql
+set define on
+Prompt confirmando cambios 3 primeras tablas
+
+
+whenever sqlerror exit rollback
+set define off
+Prompt realizando la carga de datos
 @@s-10-recurso.sql
+Prompt confirmando cambios recurso
+
+whenever sqlerror exit rollback
+set define off
+Prompt realizando la carga de datos
 @@s-10-historico-recurso-status.sql
 @@s-10-libro.sql
 @@s-10-libro-autor.sql
-@@s-10-palabra_clave.sql
+@@s-10-palabra-clave.sql
 @@s-10-palabra-clave-recurso.sql
 @@s-10-recurso-prestamo.sql
 @@s-10-revista.sql
 @@s-10-tesis.sql
 set define on
-Prompt confirmando cambios
+Prompt confirmando cambios segunda parte
+
 commit;
 
