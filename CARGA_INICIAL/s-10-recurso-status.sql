5f41b0cecee73c52141d1c9ba9804a2ccb298d39
BettySanchez7 <31338064+BettySanchez7@users.noreply.github.com>
2020-06-19 19:31:48 -0500

    Update s-10-recurso-status.sql

--- CARGA_INICIAL/s-10-recurso-status.sql
+++ CARGA_INICIAL/s-10-recurso-status.sql
@@ -1,4 +1,4 @@
-insert into RECURSO_STATUS (RECURSO_STATUS_ID, CLAVE, DESCRIPCION) values (recurso_status_seq.nextval, 'DP' 'Disponible para prestamo');
+insert into RECURSO_STATUS (RECURSO_STATUS_ID, CLAVE, DESCRIPCION) values (recurso_status_seq.nextval, 'DP', 'Disponible para prestamo');
 insert into RECURSO_STATUS (RECURSO_STATUS_ID, CLAVE, DESCRIPCION) values (recurso_status_seq.nextval, 'P', 'En prestamo');
 insert into RECURSO_STATUS (RECURSO_STATUS_ID, CLAVE, DESCRIPCION) values (recurso_status_seq.nextval, 'C', 'Solo de consulta');
 insert into RECURSO_STATUS (RECURSO_STATUS_ID, CLAVE, DESCRIPCION) values (recurso_status_seq.nextval, 'D', 'Dañado');
