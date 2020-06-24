1092dc2f39458866e953547b584f1e01e20719d8
Carlos Morales <carlosgl.moralestz@gmail.com>
2020-06-23 19:03:32 -0500

    Corrección de errores en generación de respaldos

--- RESPALDOS/s-14-generar-respaldos.sh
+++ RESPALDOS/s-14-generar-respaldos.sh
@@ -23,15 +23,39 @@ run {
 }
 EOF
 
+cat <<'EOF' > /u01/app/oracle/oradata/MOSAproy/Scripts/run_0.sh
+#!/bin/bash
+ORACLE_SID=mosaproy; export ORACLE_SID
+ORACLE_TERM=xterm; export ORACLE_TERM
+ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1; export ORACLE_HOME
+PATH=/usr/sbin:$PATH; export PATH
+PATH=$ORACLE_HOME/bin:$PATH; export PATH
+LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib; export LD_LIBRARY_PATH
+CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib; export CLASSPATH
+
+rman target / @'/u01/app/oracle/oradata/MOSAproy/Scripts/respaldo_nivel_0.rman'
+EOF
+
+cat <<'EOF' > /u01/app/oracle/oradata/MOSAproy/Scripts/run_1.sh
+#!/bin/bash
+ORACLE_SID=mosaproy; export ORACLE_SID
+ORACLE_TERM=xterm; export ORACLE_TERM
+ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1; export ORACLE_HOME
+PATH=/usr/sbin:$PATH; export PATH
+PATH=$ORACLE_HOME/bin:$PATH; export PATH
+LD_LIBRARY_PATH=$ORACLE_HOME/lib:/lib:/usr/lib; export LD_LIBRARY_PATH
+CLASSPATH=$ORACLE_HOME/jlib:$ORACLE_HOME/rdbms/jlib; export CLASSPATH
+
+rman target / @'/u01/app/oracle/oradata/MOSAproy/Scripts/respaldo_nivel_1.rman'
+EOF
+
+
 chown -R oracle:oinstall /u01/app/oracle/oradata/MOSAproy/Scripts
 chmod -R 755 /u01/app/oracle/oradata/MOSAproy/Scripts
 
-cat <<'EOF' >> /etc/crontab
-0   0   *   *   0   oracle   export ORACLE_SID=mosaproy; $ORACLE_HOME/bin/rman target / cmdfile=/u01/app/oracle/oradata/MOSAproy/Scripts/respaldo_nivel_0.rman
-0   0   *   *   1   oracle   export ORACLE_SID=mosaproy; $ORACLE_HOME/bin/rman target / cmdfile=/u01/app/oracle/oradata/MOSAproy/Scripts/respaldo_nivel_1.rman  
-0   0   *   *   2   oracle   export ORACLE_SID=mosaproy; $ORACLE_HOME/bin/rman target / cmdfile=/u01/app/oracle/oradata/MOSAproy/Scripts/respaldo_nivel_1.rman
-0   0   *   *   3   oracle   export ORACLE_SID=mosaproy; $ORACLE_HOME/bin/rman target / cmdfile=/u01/app/oracle/oradata/MOSAproy/Scripts/respaldo_nivel_1.rman
-0   0   *   *   4   oracle   export ORACLE_SID=mosaproy; $ORACLE_HOME/bin/rman target / cmdfile=/u01/app/oracle/oradata/MOSAproy/Scripts/respaldo_nivel_1.rman
-0   0   *   *   5   oracle   export ORACLE_SID=mosaproy; $ORACLE_HOME/bin/rman target / cmdfile=/u01/app/oracle/oradata/MOSAproy/Scripts/respaldo_nivel_1.rman
-0   0   *   *   6   oracle   export ORACLE_SID=mosaproy; $ORACLE_HOME/bin/rman target / cmdfile=/u01/app/oracle/oradata/MOSAproy/Scripts/respaldo_nivel_1.rman 
+cat <<'EOF' > /var/spool/cron/oracle
+0 0 * * 0 /u01/app/oracle/oradata/MOSAproy/Scripts/run_0.sh
+0 0 * * 1,2,3,4,5,6 /u01/app/oracle/oradata/MOSAproy/Scripts/run_1.sh 
 EOF
+
+chmod 600 /var/spool/cron/oracle
